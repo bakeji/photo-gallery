54ad7cb3599c6371b33b31901abd5f186127f2c2
bakeji <fatimohbakare3@gmail.com>
2023-09-21 02:41:40 +0100

    updated firebase.js file

--- src/firebase.js
+++ src/firebase.js
@@ -14,7 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-console.log(app)
 export default app;
 export const auth = getAuth(app);
-console.log(auth)
+
